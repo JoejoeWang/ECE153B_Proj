862850435349e2e32cfa666ca0659cc7ef4c8213
JoejoeWang <49485325+JoejoeWang@users.noreply.github.com>
2022-03-11 22:55:15 -0800

    Add files via upload

--- thermal1.c
+++ thermal1.c
@@ -1,31 +1,52 @@
 #include "stm32l476xx.h"
+#include "LED.h"
+#include "SPI.h"
+#include "SysClock.h"
+#include "SysTimer.h"
 
 void Thermal1_Init() {
-	// Enable HSI
-	RCC->CR |= ((uint32_t)RCC_CR_HSION);
-	while ( (RCC->CR & (uint32_t) RCC_CR_HSIRDY) == 0 );
+	    // Enable HSI
+    RCC->CR |= ((uint32_t)RCC_CR_HSION);
+    while ( (RCC->CR & (uint32_t) RCC_CR_HSIRDY) == 0 );
 
-	// Select HSI as system clock source
-	RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
-	RCC->CFGR |= (uint32_t)RCC_CFGR_SW_HSI;
-	while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) == 0 );
+    // Select HSI as system clock source
+    RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
+    RCC->CFGR |= (uint32_t)RCC_CFGR_SW_HSI;
+    while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) == 0 );
 
-	// Enable GPIO Clock
-	RCC->AHB2ENR |=	RCC_AHB2ENR_GPIOBEN;
+    // Enable GPIO Clock
+		RCC->AHB2ENR |=	RCC_AHB2ENR_GPIOBEN;
 	
 	//initialize PB6 for Thermal
-	//Configure PB3 to be used as input mode function
+	//Configure PB6 to be used as input mode function
 	GPIOB->MODER &= ~GPIO_MODER_MODE3;
 
-	//Set PB3 to no pull-up, no pull-down
+	//Set PB6 to no pull-up, no pull-down
 	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD3;
 }
 
 
-int main(void) {	
-	Thermal1_Init();
-	uint32_t mask = 1UL<<3;
-	while(1){	
-		uint32_t Thermal1 = (GPIOB->IDR & mask) == mask;
-	}
-}
+//int main(void) {	
+//	Thermal1_Init();
+//	
+//	System_Clock_Init();   // System Clock = 80 MHz
+//	SysTick_Init();
+//	
+//	SPI2_GPIO_Init();
+//	SPI2_Init();
+//	
+//	LED_MATRIX_Init();
+
+//		uint32_t mask = 1UL<<3;
+
+//		while(1){	
+//			uint32_t thermal1 = (GPIOB->IDR & mask) == mask;
+//			
+//			if(thermal1 == 1){
+//				LED_Default();
+//			}else{
+//				LED_1();
+//				
+//			}
+//		}
+//}
