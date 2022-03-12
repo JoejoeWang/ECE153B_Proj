cb30263d05c30afa5d6b90d4e7c20ce5e8b6d13a
liminding-UCSB <67501890+liminding-UCSB@users.noreply.github.com>
2022-03-11 20:15:50 -0800

    Add files via upload

---
+++ LED_MATRIX/LED.h
@@ -0,0 +1,33 @@
+/*
+ * ECE 153B - Winter 2021
+ *
+ * Name(s):
+ * Section:
+ * Lab: 4C
+ */
+
+#ifndef __STM32L476G_DISCOVERY_LED_H
+#define __STM32L476G_DISCOVERY_LED_H
+
+#include "stm32l476xx.h"
+
+void LED_Init(void);
+void LED_Default(void);
+void LED_1(void);
+void LED_2(void);
+void LED_3(void);
+void LED_4(void);
+void LED_5(void);
+
+
+void Detect_1(void);
+void Detect_2(void);
+void Detect_3(void);
+void Detect_4(void);
+void Detect_5(void);
+
+	
+void LED_send(uint16_t cmd_data);
+void Delay(void);
+
+#endif
