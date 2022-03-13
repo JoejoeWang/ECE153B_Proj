31d83aa1a430864d6ca2e645a186f427b7b4b3c6
JoejoeWang <49485325+JoejoeWang@users.noreply.github.com>
2022-03-12 21:40:55 -0800

    Add files via upload

--- buzzer.c
+++ buzzer.c
@@ -1,21 +1,21 @@
 #include "buzzer.h"
 
 void Buzzer_Trigger_Setup() {
-	// Setup PA9
+	// Setup PA10
 	
 	// Enable the clock for GPIO Port A
 	RCC->AHB2ENR |=	RCC_AHB2ENR_GPIOAEN;
-	GPIOA->MODER &= ~GPIO_MODER_MODE9;
-	GPIOA->MODER |= GPIO_MODER_MODE9_1;
-	// Configure PA9 to be used as alternative function TIM1 CH2
-	GPIOA->AFR[1] |= GPIO_AFRH_AFSEL9_0;
-	GPIOA->AFR[1] &= ~GPIO_AFRH_AFSEL9_1;
-	// Set PA9 to no pull-up, no pull-down
-	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD9;
+	GPIOA->MODER &= ~GPIO_MODER_MODE10;
+	GPIOA->MODER |= GPIO_MODER_MODE10_1;
+	// Configure PA10 to be used as alternative function TIM1 CH2
+	GPIOA->AFR[1] |= GPIO_AFRH_AFSEL10_0;
+	GPIOA->AFR[1] &= ~GPIO_AFRH_AFSEL10_1;
+	// Set PA10 to no pull-up, no pull-down
+	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD10;
 	// Set the output type of PA9 to push-pull
-	GPIOA->OTYPER &= ~GPIO_OTYPER_OT9;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT10;
 	// Set PA9 to very high output speed
-	GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED9;
+	GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED10;
 	
 	//Setup Timer 1
 	
@@ -28,11 +28,11 @@ void Buzzer_Trigger_Setup() {
 	//Set the auto reload value to maximum
 	TIM1->ARR |= 0xFFFF;
 	//Set the CCR value
-	TIM1->CCR2 |= 0x8000;
+	TIM1->CCR3 |= 0x8000;
 	//Set the output compare mode in PWM mode 1 	//Enable output compare preload
-	TIM1->CCMR1 |= TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2PE;
+	TIM1->CCMR2 |= TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC3M_2 | TIM_CCMR2_OC3PE;
 	//Enable the output in the capture/compare enable register
-	TIM1->CCER |= TIM_CCER_CC2E;
+	TIM1->CCER |= TIM_CCER_CC3E;
 	//Break and Dead-time register
 	//Main output enable	//off-state selection for run mode
 	TIM1->BDTR |= TIM_BDTR_MOE | TIM_BDTR_OSSR;
@@ -45,7 +45,6 @@ void Buzzer_Trigger_Setup() {
 	//Set the direction of the counter to downcounter	//Enable the counter
 	TIM1->CR1 |= TIM_CR1_DIR | TIM_CR1_CEN;
 }
-
 
 void Buzzer_On(void) {	
 	// Enable High Speed Internal Clock (HSI = 16 MHz)
